1351194579c0e65b580d648dc7fc9ff58a1d093a
wangzhongyu <wanghzhongyu1@xiaomi.com>
2021-04-23 22:19:33 +0800

    rename

--- src/main/java/com/example/demo/leetcode/BuildTree_105.java
+++ src/main/java/com/example/demo/leetcode/BuildTree_105.java
@@ -6,26 +6,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class BuildTree_105 {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
+    public TreeNode buildTree(int[] preOrder, int[] inorder) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i=0;i<inorder.length;i++) {
             map.put(inorder[i], i);
         }
 
-        return build(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+        return build(preOrder, 0, preOrder.length - 1, inorder, 0, inorder.length - 1, map);
     }
 
-    private TreeNode build(int[] preorder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd, Map<Integer, Integer> map) {
+    private TreeNode build(int[] preOrder, int pStart, int pEnd, int[] inorder, int iStart, int iEnd, Map<Integer, Integer> map) {
         if (pStart > pEnd) {
             return null;
         }
 
-        int root = preorder[pStart];
+        int root = preOrder[pStart];
         TreeNode treeNode = new TreeNode(root);
         int index = map.get(root);
         int len = index - iStart;
-        treeNode.left = build(preorder, pStart + 1, pStart + len, inorder, iStart, index - 1, map);
-        treeNode.right = build(preorder, pStart + len + 1, pEnd, inorder, index + 1, iEnd, map);
+        treeNode.left = build(preOrder, pStart + 1, pStart + len, inorder, iStart, index - 1, map);
+        treeNode.right = build(preOrder, pStart + len + 1, pEnd, inorder, index + 1, iEnd, map);
 
         return treeNode;
     }
